a3405c4accd8ab2b8c9687921523dad428abcda6
JEHAD HAMAD <5088836@nwpolytech.ca>
2024-10-02 09:44:37 -0600

    just changed the names of my varibles, and removed my varible turns as it did nothing at all

--- Proj1/crapsGame.cpp
+++ Proj1/crapsGame.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <time.h>
 using namespace std;
-int cred = 100; 
+int credits = 100; 
 
 static int die() {
 	int d1 = rand() % 6 + 1;
@@ -17,65 +17,66 @@ static int die() {
 static void playGame() {
 	
 	int wager = 0;
-	int plyPoint = 0;
-	int turns = 1;
-	bool state = true;
-	cout << "You have " << cred << " credits ";
+	int playPoint = 0;
+	
+	bool playing = true;
+	cout << "You have " << credits << " credits ";
 	cout << "How much do you wager ";
 
 
 	do {
 		cin >> wager;
-		if (wager <= 0 || wager > cred) {
+		if (wager <= 0 || wager > credits) {
 			cout << "Invalid wager. Try again please: ";
 		}
-	} while (wager <= 0 || wager > cred);
+	} while (wager <= 0 || wager > credits);
 
-	while (state) {
+	while (playing) {
 		cout << endl;
-		int num = die();
-		if (((num == 7) || (num == 11)) && (turns == 1)) {
-			cred += wager;
+		int Throw = die();
+
+		if ((Throw == 7) || (Throw == 11)) {
+			credits += wager;
 			
-			cout << "\nplayer wins \n\nyou won " << wager << " Your new balance is " << cred << " credits";
-			state = false;
+			cout << "\nplayer wins \n\nyou won " << wager << " Your new balance is " << credits << " credits";
+			playing = false;
 		}
-		else if (((num == 2) || (num == 3) || (num == 12)) && (turns == 1)) {
-			cred -= wager;
+		else if ((Throw == 2) || (Throw == 3) || (Throw == 12)) {
+			credits -= wager;
 			
-			if (cred == 0) {
+			if (credits == 0) {
 				cout << "\nyou lost " << wager << " credits You are out of credits - game over";
 
 			}
 			else {
-				cout << "player loses\n\nyou lost " << wager << " Your new balance is " << cred << " credits";
+				cout << "player loses\n\nyou lost " << wager << " Your new balance is " << credits << " credits";
 			}
-			state = false;
+			playing = false;
 		}
 		else {
-			plyPoint = num;
-			cout << "point is " << plyPoint << "\n";
-			int newnum = 0;
-			while (state) {
-				newnum = die();
+			playPoint = Throw;
+			cout << "point is " << playPoint << "\n";
+			int newThrow = 0;
+			while (playing) {
+				newThrow = die();
 
-				if (newnum == 7) {
-					cred -= wager;
+				if (newThrow == 7) {
+					credits -= wager;
 					
-					if (cred == 0) {
+					if (credits == 0) {
 						cout << "\nyou lost " << wager << " credits You are out of credits - game over";
 
 					}
 					else {
-						cout << "player loses\n\nyou lost " << wager << " Your new balance is " << cred << " credits";
+						cout << "player loses\n\nyou lost " << wager << " Your new balance is " << credits << " credits";
 					}
-					state = false;
+					playing = false;
 				}
-				else if (newnum == plyPoint) {
-					cred += wager;
+				else if (newThrow == playPoint) {
+					credits += wager;
 					
-					cout << "player wins\n\nyou won " << wager << " Your new balance is " << cred << " credits";
-					state = false;
+					cout << "player wins\n\nyou won " << wager << " Your new balance is " << credits << " credits";
+					playing = false;
 				}
 
 			}
@@ -92,7 +93,7 @@ int main() {
 	bool done = false;
 	while (!done) {
 		playGame();
-		if (cred == 0) {
+		if (credits == 0) {
 			break;
 		}
 		cout << " Play again (y/n) ? ";
