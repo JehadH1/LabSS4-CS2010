4486c99218485fc766cbfa9511dc72c70bc8a5a7
JEHAD HAMAD <5088836@nwpolytech.ca>
2024-10-08 12:05:09 -0600

    idk

--- Proj2/gameOfLife.cpp
+++ Proj2/gameOfLife.cpp
@@ -1,10 +1,35 @@
-
+#include<vector>
 #include <iostream>
 using namespace std;
 
+using VEC = vector<int>;
+using MATRIX = vector<VEC>;
+//make a matrix
+static MATRIX makeMat() {
+    int col, row = 0;
+    cout << "how big do your want your matrix to be?";
+    cout << "your row is: ";
+    cin >> row;
+    cout << "your coloumn is: ";
+    cin >> col;
+
+    return MATRIX m(row, VEC(col, 0));
+}
+//place cells
+static MATRIX placeCells() {
+
+}
+//printing my matrix
+static void printMat() {
+    makeMat();
+
+
+}
+
+
 int main()
 {
-    cout << "this is my hame of life\n";
+    printMat();
 }
 
 
