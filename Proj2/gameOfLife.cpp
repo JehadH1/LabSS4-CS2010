1a88882f8b9c53038ab3a7102074632b01a06487
JEHAD HAMAD <5088836@nwpolytech.ca>
2024-10-09 14:10:08 -0600

    just to save

--- Proj2/gameOfLife.cpp
+++ Proj2/gameOfLife.cpp
@@ -5,56 +5,102 @@ using namespace std;
 using VEC = vector<int>;
 using MATRIX = vector<VEC>;
 //make a matrix
-static MATRIX makeMat(int &row, int &col) {
+static MATRIX makeMat() {
+    int row, col = 0;
     cout << "how big do your want your matrix to be? \n";
     cout << "your row is: ";
     cin >> row;
     cout << "your coloumn is: ";
     cin >> col;
-    MATRIX m(row, VEC(col, 0));
+    MATRIX currentMat(row+2, VEC(col+2, 0));
     cout << endl;
-    return m;
+    return currentMat;
 }
 //printing my matrix
-static void printMat(MATRIX curr) {
-
-    for (auto y = 0; y < curr[0].size(); y++) {
-        for (auto x = 0; x < curr[y].size(); x++) {
-            cout << curr[y][x];
+static void printMat(MATRIX currentMat) {
+    for (auto y = 2; y < currentMat[0].size(); y++) {
+        for (auto x = 2; x < currentMat[y].size(); x++) {
+            cout << currentMat[y][x];
         }
         cout << endl;
     }
     cout << endl;
 }
 //place cells
-static void placeCells(MATRIX &curr) {
+static void placeCells(MATRIX &currentMat) {
     cout << "where do you want to place your cells: ";
     int col, row, cell = 0;
     cin >> cell;
     for (auto i = 0; i < cell; i++) {
         cin >> row;
         cin >> col;
-        curr[row][col] = 1;
+        currentMat[row+2][col+2] = 1;
     }
     cout << endl;
 }
 //rules
-static void rules(MATRIX &curr, int &row, int &col) {
-    for (auto y = 0; y < curr[0].size(); y++) {
-        for (auto x = 0; x < curr[y].size(); x++) {
-            
-        } 
-    } 
-}
+static void rules(MATRIX& copyMat, MATRIX& currentMat) {
+ 
+    for (auto y = 2; y < currentMat[0].size(); y++) {
+        int i = 0;
+        for (auto x = 2; x < currentMat[y].size(); x++) {
 
+            if (currentMat[y - 1][x] == 1)      //checks up
+                i += 1;
+
+            if (currentMat[y + 1][x] == 1)      //checks down
+                i += 1;
+
+            if (currentMat[y][x - 1] == 1)        // checks left
+                i += 1;
+
+            if (currentMat[y][x + 1] == 1)        //checks right
+                i += 1;
+
+            if (currentMat[y - 1][x - 1] == 1)    //checks left upper corner
+                i += 1;
+
+            if (currentMat[y - 1][x + 1] == 1)  //checks right upper corner
+                i += 1;
+
+            if (currentMat[y + 1][x - 1] == 1)  //checks left lower corner
+                i += 1;
+
+            if (currentMat[y + 1][x + 1] == 1)  //checks right lower corner
+                i += 1;
+
+            if (currentMat[y][x] == 1) {
+                if (i < 2) {
+                    copyMat[y][x] = 0;
+                }
+                else if ((i == 2) || (i == 3)) {
+                    copyMat[y][x] = 1;
+                }
+                else {
+                    copyMat[y][x] = 0;
+                }
+            }
+            else if (currentMat[y][x] == 0) {
+                if (i == 3) {
+                    copyMat[y][x] = 1;
+                }
+                else {
+                    copyMat[y][x] = 0;
+                }
+            }
+        }
+
+    }
+}
 
 int main()
 {
-    int row, col = 0;
-    MATRIX currentMat = makeMat(row, col);
+    MATRIX currentMat = makeMat();
     printMat(currentMat);
     placeCells(currentMat);
     printMat(currentMat);
+    MATRIX copyMat = currentMat;
+    //rules(copyMat, currentMat);
 
 }
 
