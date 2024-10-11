25f4dab29b03e1445a8707a9b5210f411d71ca0c
JEHAD HAMAD <5088836@nwpolytech.ca>
2024-10-11 11:43:07 -0600

    fixed everything i think

--- Proj1/crapsGame.cpp
+++ Proj1/crapsGame.cpp
@@ -5,27 +5,24 @@
 using namespace std;
 
 static int die();
-static void playGame(int &credits);
+static void playGame(int& credits);
 static void winner(int& credits, int& wager, bool& playing);
 static void loser(int& credits, int& wager, bool& playing);
 
+//Main
 int main() {
 	int credits = 100;
-	//My main which will call my playGame function
 	srand(time(NULL));
 	string ans = "n";
 	bool done = false;
-	//this while loop is used to see of you are done playing or not;
+
 	while (!done) {
 		playGame(credits);
-		//this part of the loop is used to break out of the game if you have zero credits as you cant play again
 		if (credits == 0) {
 			break;
 		}
 
 		cout << " Play again (y/n) ? ";
-		//while loop here will loop till you give it a string thats a y or n.
-		//if you dont it will keep you here.
 		while (ans != "n" || ans != "N" || ans != "y" || ans != "y") {
 			cin >> ans;
 			if (ans == "n" || ans == "N") {
@@ -45,23 +42,25 @@ int main() {
 	return 0;
 }
 
+//function for dice roll that will roll two dice and return thier some
 static int die() {
-	//function for dice roll that will roll two dice and return thier some
 	int d1 = rand() % 6 + 1;
 	int d2 = rand() % 6 + 1;
 	int sum = d1 + d2;
 	cout << "player rolled " << d1 << " + " << d2 << " = " << sum << endl;
 	return sum;
 }
+
+//does winner stuff
 static void winner(int& credits, int& wager, bool& playing) {
 	credits += wager;
-
 	cout << "player wins\n\nyou won " << wager << " Your new balance is " << credits << " credits";
 	playing = false;
 }
+
+//does loser stuff
 static void loser(int& credits, int& wager, bool& playing) {
 	credits -= wager;
-	//checks if you lost all your credits
 	if (credits == 0) {
 		cout << "\nyou lost " << wager << " credits You are out of credits - game over";
 	}
@@ -71,8 +70,8 @@ static void loser(int& credits, int& wager, bool& playing) {
 	playing = false;
 }
 
-static void playGame(int &credits) {
-	//function for playGame, will implement the rules for craps
+//function for playGame, will implement the rules for craps
+static void playGame(int& credits) {
 	int wager = 0;
 	int playPoint = 0;
 	bool playing = true;
@@ -80,20 +79,15 @@ static void playGame(int &credits) {
 	cout << "You have " << credits << " credits \n";
 	cout << "How much do you wager: ";
 
-	//wager part of my game, will chech if you gave it the right values if not will continue to loop till you do.
-	//if you give it a double, will cast it as a int and let you play.
 	while (wager <= 0 || wager > credits) {
 		cin >> wager;
 		if (wager <= 0 || wager > credits) {
-			//if you eneter a letter will clear and ignore your input
 			cin.clear();
 			cin.ignore();
 			cout << "Invalid wager. Try again please: ";
 		}
 	}
 
-	//main body for the game, while loop continues as long as you play
-	//the if and else if part are used to check for the instant wins
 	while (playing) {
 		cout << endl;
 		int roll = die();
@@ -104,13 +98,10 @@ static void playGame(int &credits) {
 		else if ((roll == 2) || (roll == 3) || (roll == 12)) {
 			loser(credits, wager, playing);
 		}
-		//This else if used to make you sum the play point if you didnt instantly win.
 		else {
 			playPoint = roll;
 			cout << "point is " << playPoint << "\n";
 			int newRoll = 0;
-			//While loop will continue to go till you reroll your setpoint which means you win
-			//Or you roll a seven which means you have lost
 			while (playing) {
 				newRoll = die();
 
@@ -124,4 +115,3 @@ static void playGame(int &credits) {
 		}
 	}
 }
-
